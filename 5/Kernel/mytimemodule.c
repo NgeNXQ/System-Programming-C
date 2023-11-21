6d8a0dc2bbb1ecbc6558c4cd89a3a042af99dba6
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2023-11-21 06:46:34 +0200

    Update mytimemodule.c

--- 5/Kernel/mytimemodule.c
+++ 5/Kernel/mytimemodule.c
@@ -20,7 +20,7 @@ MODULE_DESCRIPTION("Kernel module to provide the current time");
 #define print_message(msg, ...) printk(KERN_ERR MSG_PREF msg, ##__VA_ARGS__);
 
 dev_t device_number = 0;
-static struct cdev character_device;
+static struct cdev labwork5_device;
 static struct device* device = NULL;
 static struct class* device_class = NULL;
 
@@ -29,70 +29,70 @@ static int release(struct inode*, struct file*);
 static ssize_t read(struct file*, char __user*, size_t, loff_t*);
 
 static struct file_operations fops = {
-	.read = read,
-	.open = open,
-	.release = release
+    .read = read,
+    .open = open,
+    .release = release
 };
 
 static int open(struct inode* inode, struct file* file)
 {
-	print_message("File has been opened\n");
-	return 0;
+    print_message("File has been opened\n");
+    return 0;
 }
 
 static ssize_t read(struct file* file, char __user* buffer, size_t count, loff_t* offset)
 {
-	const int TIMEZONE_OFFSET = 2;
-	char data[BUFFER_LENGTH] = { 0 };
+    const int TIMEZONE_OFFSET = 2;
+    char data[BUFFER_LENGTH] = { 0 };
 
-	struct tm tm_info_utc;
-	time64_to_tm(ktime_get_real_seconds(), 0, &tm_info_utc);
+    struct tm tm_info_utc;
+    time64_to_tm(ktime_get_real_seconds(), 0, &tm_info_utc);
 
-	tm_info_utc.tm_hour += TIMEZONE_OFFSET;
+    tm_info_utc.tm_hour += TIMEZONE_OFFSET;
 
-	if (tm_info_utc.tm_hour >= 24)
-		tm_info_utc.tm_hour -= 24;
+    if (tm_info_utc.tm_hour >= 24)
+        tm_info_utc.tm_hour -= 24;
 
-	snprintf(data, BUFFER_LENGTH, "%02d:%02d:%02d", tm_info_utc.tm_hour, tm_info_utc.tm_min, tm_info_utc.tm_sec);
+    snprintf(data, BUFFER_LENGTH, "%02d:%02d:%02d", tm_info_utc.tm_hour, tm_info_utc.tm_min, tm_info_utc.tm_sec);
 
-	if (count > BUFFER_LENGTH)
-		count = BUFFER_LENGTH;
+    if (count > BUFFER_LENGTH)
+        count = BUFFER_LENGTH;
 
-	if (copy_to_user(buffer, data, count))
-		return -EFAULT;
+    if (copy_to_user(buffer, data, count))
+        return -EFAULT;
 
-	return count;
+    return count;
 }
 
 static int release(struct inode* inode, struct file* file)
 {
-	print_message("File has been released\n");
-	return 0;
+    print_message("File has been released\n");
+    return 0;
 }
 
-static int __init mytimemodule_init(void)
+static int __init mytimemodule_init(void) 
 {
 	int result = 0;
 
-	if ((result = alloc_chrdev_region(&device_number, 0, 1, "character_device")) < 0)
+	if((result = alloc_chrdev_region(&device_number, 0, 1, "labwork5_device")) < 0)
 	{
 		print_message("Cannot allocate major number\n");
 		return result;
 	}
-
-	cdev_init(&character_device, &fops);
+	
+	cdev_init(&labwork5_device, &fops);
 	print_message("Major = %d Minor = %d\n", MAJOR(device_number), MINOR(device_number));
 
-	if ((result = cdev_add(&character_device, device_number, 1)) < 0)
+	if((result = cdev_add(&labwork5_device, device_number, 1)) < 0)
 	{
 		print_message("Cannot add the device to the system\n");
 		unregister_chrdev_region(device_number, 1);
 		return result;
 	}
 
-	device_class = class_create(THIS_MODULE, "my_class");
+	device_class = class_create(THIS_MODULE, "device_class");
 
-	if (IS_ERR(device_class))
+	if(IS_ERR(device_class))
 	{
 		result = PTR_ERR(device_class);
 		unregister_chrdev_region(device_number, 1);
@@ -100,9 +100,9 @@ static int __init mytimemodule_init(void)
 		return result;
 	}
 
-	device = device_create(device_class, NULL, device_number, NULL, "my_cdev");
+	device = device_create(device_class, NULL, device_number, NULL, "labwork5_device");
 
-	if (IS_ERR(device))
+	if(IS_ERR(device))
 	{
 		result = PTR_ERR(device);
 		class_destroy(device_class);
@@ -115,9 +115,9 @@ static int __init mytimemodule_init(void)
 	return 0;
 }
 
-static void __exit mytimemodule_exit(void)
+static void __exit mytimemodule_exit(void) 
 {
-	cdev_del(&character_device);
+	cdev_del(&labwork5_device);
 	class_destroy(device_class);
 	unregister_chrdev_region(device_number, 1);
 	device_destroy(device_class, device_number);
