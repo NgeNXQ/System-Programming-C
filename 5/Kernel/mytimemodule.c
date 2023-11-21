d05233cf2922ce489f2844a52d9e0b3643e4a1b1
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2023-11-21 06:17:57 +0200

    Update mytimemodule.c

--- 5/Kernel/mytimemodule.c
+++ 5/Kernel/mytimemodule.c
@@ -1,11 +1,12 @@
-#include <linux/kernel.h>
-#include <linux/module.h>
-
+#include <linux/fs.h>
 #include <linux/time.h>
 #include <linux/cdev.h>
 #include <linux/init.h>
 #include <linux/ktime.h>
+#include <linux/kdev_t.h>
 #include <linux/device.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/uaccess.h>
 #include <linux/timekeeping.h>
 
@@ -27,7 +28,7 @@ static int open(struct inode*, struct file*);
 static int release(struct inode*, struct file*);
 static ssize_t read(struct file*, char __user*, size_t, loff_t*);
 
-static struct file_operations fops = { 
+static struct file_operations fops = {
 	.read = read,
 	.open = open,
 	.release = release
@@ -42,17 +43,17 @@ static int open(struct inode* inode, struct file* file)
 static ssize_t read(struct file* file, char __user* buffer, size_t count, loff_t* offset)
 {
 	const int TIMEZONE_OFFSET = 2;
-    char data[BUFFER_LENGTH] = { 0 };
+	char data[BUFFER_LENGTH] = { 0 };
 
-    struct tm tm_info_utc;
-    time64_to_tm(ktime_get_real_seconds(), 0, &tm_info_utc);
+	struct tm tm_info_utc;
+	time64_to_tm(ktime_get_real_seconds(), 0, &tm_info_utc);
 
-    tm_info_utc.tm_hour += TIMEZONE_OFFSET;
-    	
-    if (tm_info_utc.tm_hour >= 24) 
-        tm_info_utc.tm_hour -= 24;
+	tm_info_utc.tm_hour += TIMEZONE_OFFSET;
 
-    snprintf(data, BUFFER_LENGTH, "%02d:%02d:%02d", tm_info_utc.tm_hour, tm_info_utc.tm_min, tm_info_utc.tm_sec);
+	if (tm_info_utc.tm_hour >= 24)
+		tm_info_utc.tm_hour -= 24;
+
+	snprintf(data, BUFFER_LENGTH, "%02d:%02d:%02d", tm_info_utc.tm_hour, tm_info_utc.tm_min, tm_info_utc.tm_sec);
 
 	if (count > BUFFER_LENGTH)
 		count = BUFFER_LENGTH;
@@ -69,53 +70,52 @@ static int release(struct inode* inode, struct file* file)
 	return 0;
 }
 
-static int __init mytimemodule_init(void) 
+static int __init mytimemodule_init(void)
 {
-	long res = 0;
+	int result = 0;
 
-	if ((res = alloc_chrdev_region(&device_number, 0, 1, "character_device")) < 0)
+	if ((result = alloc_chrdev_region(&device_number, 0, 1, "character_device")) < 0)
 	{
 		print_message("Cannot allocate major number\n");
-		return res;
+		return result;
 	}
 
+	cdev_init(&character_device, &fops);
 	print_message("Major = %d Minor = %d\n", MAJOR(device_number), MINOR(device_number));
 
-	cdev_init(&character_device, &fops);
-
-	if ((res = cdev_add(&character_device, device_number, 1)) < 0)
+	if ((result = cdev_add(&character_device, device_number, 1)) < 0)
 	{
 		print_message("Cannot add the device to the system\n");
 		unregister_chrdev_region(device_number, 1);
-		return res;
+		return result;
 	}
 
-	device_class = class_create(THIS_MODULE, "device_class");
+	device_class = class_create(THIS_MODULE, "my_class");
 
-	if (IS_ERR(dev_class)) 
+	if (IS_ERR(device_class))
 	{
-		res = PTR_ERR(device_class);
-		print_message("Cannot create the struct class\n");
+		result = PTR_ERR(device_class);
 		unregister_chrdev_region(device_number, 1);
-		return res;
+		print_message("Cannot create the struct class\n");
+		return result;
 	}
 
-	device = device_create(device_class, NULL, device_number, NULL, "character_device");
+	device = device_create(device_class, NULL, device_number, NULL, "my_cdev");
 
-	if (IS_ERR(labwork5_device))
+	if (IS_ERR(device))
 	{
-		res = PTR_ERR(device);
+		result = PTR_ERR(device);
 		class_destroy(device_class);
 		unregister_chrdev_region(device_number, 1);
 		print_message("Cannot create the Device\n");
-		return res;
+		return result;
 	}
 
-	print_message("mytimemodule has been initialized.\n");
+	print_message("mytimemodule has been allocated.\n");
 	return 0;
 }
 
-static void __exit mytimemodule_exit(void) 
+static void __exit mytimemodule_exit(void)
 {
 	cdev_del(&character_device);
 	class_destroy(device_class);
